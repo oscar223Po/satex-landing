e4eb5149f203e1177996dbaa4eb89a6c79954757
Oscar Zhantaev <oscarzhantaev@Oscars-MacBook-Air.local>
2026-04-24 00:07:53 +0600

    Integration of access-popup (v-demo) section development

--- dist/js/dev/index.js
+++ dist/js/dev/index.js
@@ -3486,6 +3486,247 @@ function initSliders() {
 }
 document.querySelector("[data-fls-slider]") && window.addEventListener("load", initSliders);
 //#endregion
+//#region src/components/layout/popup/popup.js
+var Popup = class {
+	constructor(options) {
+		let config = {
+			logging: true,
+			init: true,
+			attributeOpenButton: "data-fls-popup-link",
+			attributeCloseButton: "data-fls-popup-close",
+			fixElementSelector: "[data-fls-lp]",
+			attributeMain: "data-fls-popup",
+			youtubeAttribute: "data-fls-popup-youtube",
+			youtubePlaceAttribute: "data-fls-popup-youtube-place",
+			setAutoplayYoutube: true,
+			classes: {
+				popup: "popup",
+				popupContent: "data-fls-popup-body",
+				popupActive: "data-fls-popup-active",
+				bodyActive: "data-fls-popup-open"
+			},
+			focusCatch: true,
+			closeEsc: true,
+			bodyLock: true,
+			hashSettings: {
+				location: true,
+				goHash: true
+			},
+			on: {
+				beforeOpen: function() {},
+				afterOpen: function() {},
+				beforeClose: function() {},
+				afterClose: function() {}
+			}
+		};
+		this.youTubeCode;
+		this.isOpen = false;
+		this.targetOpen = {
+			selector: false,
+			element: false
+		};
+		this.previousOpen = {
+			selector: false,
+			element: false
+		};
+		this.lastClosed = {
+			selector: false,
+			element: false
+		};
+		this._dataValue = false;
+		this.hash = false;
+		this._reopen = false;
+		this._selectorOpen = false;
+		this.lastFocusEl = false;
+		this._focusEl = [
+			"a[href]",
+			"input:not([disabled]):not([type=\"hidden\"]):not([aria-hidden])",
+			"button:not([disabled]):not([aria-hidden])",
+			"select:not([disabled]):not([aria-hidden])",
+			"textarea:not([disabled]):not([aria-hidden])",
+			"area[href]",
+			"iframe",
+			"object",
+			"embed",
+			"[contenteditable]",
+			"[tabindex]:not([tabindex^=\"-\"])"
+		];
+		this.options = {
+			...config,
+			...options,
+			classes: {
+				...config.classes,
+				...options?.classes
+			},
+			hashSettings: {
+				...config.hashSettings,
+				...options?.hashSettings
+			},
+			on: {
+				...config.on,
+				...options?.on
+			}
+		};
+		this.bodyLock = false;
+		this.options.init && this.initPopups();
+	}
+	initPopups() {
+		this.buildPopup();
+		this.eventsPopup();
+	}
+	buildPopup() {}
+	eventsPopup() {
+		document.addEventListener("click", function(e) {
+			const buttonOpen = e.target.closest(`[${this.options.attributeOpenButton}]`);
+			if (buttonOpen) {
+				e.preventDefault();
+				this._dataValue = buttonOpen.getAttribute(this.options.attributeOpenButton) ? buttonOpen.getAttribute(this.options.attributeOpenButton) : "error";
+				this.youTubeCode = buttonOpen.getAttribute(this.options.youtubeAttribute) ? buttonOpen.getAttribute(this.options.youtubeAttribute) : null;
+				if (this._dataValue !== "error") {
+					if (!this.isOpen) this.lastFocusEl = buttonOpen;
+					this.targetOpen.selector = `${this._dataValue}`;
+					this._selectorOpen = true;
+					this.open();
+					return;
+				}
+				return;
+			}
+			if (e.target.closest(`[${this.options.attributeCloseButton}]`) || !e.target.closest(`[${this.options.classes.popupContent}]`) && this.isOpen) {
+				e.preventDefault();
+				this.close();
+				return;
+			}
+		}.bind(this));
+		document.addEventListener("keydown", function(e) {
+			if (this.options.closeEsc && e.which == 27 && e.code === "Escape" && this.isOpen) {
+				e.preventDefault();
+				this.close();
+				return;
+			}
+			if (this.options.focusCatch && e.which == 9 && this.isOpen) {
+				this._focusCatch(e);
+				return;
+			}
+		}.bind(this));
+		if (this.options.hashSettings.goHash) {
+			window.addEventListener("hashchange", function() {
+				if (window.location.hash) this._openToHash();
+				else this.close(this.targetOpen.selector);
+			}.bind(this));
+			if (window.location.hash) this._openToHash();
+		}
+	}
+	open(selectorValue) {
+		if (bodyLockStatus) {
+			this.bodyLock = document.documentElement.hasAttribute("data-fls-scrolllock") && !this.isOpen ? true : false;
+			if (selectorValue && typeof selectorValue === "string" && selectorValue.trim() !== "") {
+				this.targetOpen.selector = selectorValue;
+				this._selectorOpen = true;
+			}
+			if (this.isOpen) {
+				this._reopen = true;
+				this.close();
+			}
+			if (!this._selectorOpen) this.targetOpen.selector = this.lastClosed.selector;
+			if (!this._reopen) this.previousActiveElement = document.activeElement;
+			this.targetOpen.element = document.querySelector(`[${this.options.attributeMain}=${this.targetOpen.selector}]`);
+			if (this.targetOpen.element) {
+				const codeVideo = this.youTubeCode || this.targetOpen.element.getAttribute(`${this.options.youtubeAttribute}`);
+				if (codeVideo) {
+					const urlVideo = `https://www.youtube.com/embed/${codeVideo}?rel=0&showinfo=0&autoplay=1`;
+					const iframe = document.createElement("iframe");
+					const autoplay = this.options.setAutoplayYoutube ? "autoplay;" : "";
+					iframe.setAttribute("allowfullscreen", "");
+					iframe.setAttribute("allow", `${autoplay}; encrypted-media`);
+					iframe.setAttribute("src", urlVideo);
+					if (!this.targetOpen.element.querySelector(`[${this.options.youtubePlaceAttribute}]`)) this.targetOpen.element.querySelector("[data-fls-popup-content]").setAttribute(`${this.options.youtubePlaceAttribute}`, "");
+					this.targetOpen.element.querySelector(`[${this.options.youtubePlaceAttribute}]`).appendChild(iframe);
+				}
+				if (this.options.hashSettings.location) {
+					this._getHash();
+					this._setHash();
+				}
+				this.options.on.beforeOpen(this);
+				document.dispatchEvent(new CustomEvent("beforePopupOpen", { detail: { popup: this } }));
+				this.targetOpen.element.setAttribute(this.options.classes.popupActive, "");
+				document.documentElement.setAttribute(this.options.classes.bodyActive, "");
+				if (!this._reopen) !this.bodyLock && bodyLock();
+				else this._reopen = false;
+				this.targetOpen.element.setAttribute("aria-hidden", "false");
+				this.previousOpen.selector = this.targetOpen.selector;
+				this.previousOpen.element = this.targetOpen.element;
+				this._selectorOpen = false;
+				this.isOpen = true;
+				setTimeout(() => {
+					this._focusTrap();
+				}, 50);
+				this.options.on.afterOpen(this);
+				document.dispatchEvent(new CustomEvent("afterPopupOpen", { detail: { popup: this } }));
+			}
+		}
+	}
+	close(selectorValue) {
+		if (selectorValue && typeof selectorValue === "string" && selectorValue.trim() !== "") this.previousOpen.selector = selectorValue;
+		if (!this.isOpen || !bodyLockStatus) return;
+		this.options.on.beforeClose(this);
+		document.dispatchEvent(new CustomEvent("beforePopupClose", { detail: { popup: this } }));
+		if (this.targetOpen.element.querySelector(`[${this.options.youtubePlaceAttribute}]`)) setTimeout(() => {
+			this.targetOpen.element.querySelector(`[${this.options.youtubePlaceAttribute}]`).innerHTML = "";
+		}, 500);
+		this.previousOpen.element.removeAttribute(this.options.classes.popupActive);
+		this.previousOpen.element.setAttribute("aria-hidden", "true");
+		if (!this._reopen) {
+			document.documentElement.removeAttribute(this.options.classes.bodyActive);
+			!this.bodyLock && bodyUnlock();
+			this.isOpen = false;
+		}
+		this._removeHash();
+		if (this._selectorOpen) {
+			this.lastClosed.selector = this.previousOpen.selector;
+			this.lastClosed.element = this.previousOpen.element;
+		}
+		this.options.on.afterClose(this);
+		document.dispatchEvent(new CustomEvent("afterPopupClose", { detail: { popup: this } }));
+		setTimeout(() => {
+			this._focusTrap();
+		}, 50);
+	}
+	_getHash() {
+		if (this.options.hashSettings.location) this.hash = `#${this.targetOpen.selector}`;
+	}
+	_openToHash() {
+		let classInHash = window.location.hash.replace("#", "");
+		const openButton = document.querySelector(`[${this.options.attributeOpenButton}="${classInHash}"]`);
+		if (openButton) this.youTubeCode = openButton.getAttribute(this.options.youtubeAttribute) ? openButton.getAttribute(this.options.youtubeAttribute) : null;
+		if (classInHash) this.open(classInHash);
+	}
+	_setHash() {
+		history.pushState("", "", this.hash);
+	}
+	_removeHash() {
+		history.pushState("", "", window.location.href.split("#")[0]);
+	}
+	_focusCatch(e) {
+		const focusable = this.targetOpen.element.querySelectorAll(this._focusEl);
+		const focusArray = Array.prototype.slice.call(focusable);
+		const focusedIndex = focusArray.indexOf(document.activeElement);
+		if (e.shiftKey && focusedIndex === 0) {
+			focusArray[focusArray.length - 1].focus();
+			e.preventDefault();
+		}
+		if (!e.shiftKey && focusedIndex === focusArray.length - 1) {
+			focusArray[0].focus();
+			e.preventDefault();
+		}
+	}
+	_focusTrap() {
+		const focusable = this.previousOpen.element.querySelectorAll(this._focusEl);
+		if (!this.isOpen && this.lastFocusEl) this.lastFocusEl.focus();
+		else focusable[0].focus();
+	}
+};
+document.querySelector("[data-fls-popup]") && window.addEventListener("load", () => window.flsPopup = new Popup({}));
+//#endregion
 //#region src/components/layout/menu/menu.js
 function menuInit() {
 	document.addEventListener("click", function(e) {
