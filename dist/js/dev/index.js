15e1d4d266a756f53eb60a71bbcbf515c6ea7355
Oscar Zhantaev <oscarzhantaev@Oscars-MacBook-Air.local>
2026-04-29 21:21:27 +0600

    Making edits based on the results of layout No. 2

--- dist/js/dev/index.js
+++ dist/js/dev/index.js
@@ -3982,6 +3982,46 @@ function pageNavigation() {
 }
 document.querySelector("[data-fls-scrollto]") && window.addEventListener("load", pageNavigation);
 //#endregion
+//#region src/assets/img/logo.svg?url
+var logo_default = "" + new URL("../assets/img/logo.svg", import.meta.url).href;
+//#endregion
+//#region src/components/effects/preloader/preloader.js
+function preloader() {
+	const html = document.documentElement;
+	const modeEl = document.querySelector("[data-fls-preloader]");
+	if (!modeEl) {
+		finishPreloader(html);
+		return;
+	}
+	const once = modeEl.getAttribute("data-fls-preloader") === "true";
+	if (once && localStorage.getItem(location.href)) {
+		finishPreloader(html);
+		return;
+	}
+	const markup = `
+		<div class="fls-preloader">
+			<div class="fls-preloader__body">
+				<img class="fls-preloader__logo" src="${logo_default}" alt="Satex" width="96" height="26" decoding="async">
+			</div>
+		</div>`;
+	document.body.insertAdjacentHTML("beforeend", markup);
+	html.setAttribute("data-fls-preloader-loading", "");
+	html.setAttribute("data-fls-scrolllock", "");
+	const onWindowLoad = () => {
+		if (once) localStorage.setItem(location.href, "preloaded");
+		finishPreloader(html);
+	};
+	if (document.readyState === "complete") onWindowLoad();
+	else window.addEventListener("load", onWindowLoad, { once: true });
+}
+function finishPreloader(html) {
+	html.setAttribute("data-fls-preloader-loaded", "");
+	html.removeAttribute("data-fls-preloader-loading");
+	html.removeAttribute("data-fls-scrolllock");
+	html.setAttribute("data-fls-loaded", "");
+}
+document.addEventListener("DOMContentLoaded", preloader);
+//#endregion
 //#region src/components/effects/marquee/marquee.js
 /** Same breakpoint as styles/settings.scss $tablet with @media (width < toEm($tablet)). */
 var MARQUEE_TABLET_MAX_PX = 992;
